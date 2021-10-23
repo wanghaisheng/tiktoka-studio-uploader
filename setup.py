372e1a61b501fbdcee2b71b3e908f9f88f64fff4
wanghaisheng <edwin.uestc@gmail.com>
2021-10-23 17:25:56 +0800

    version change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import re
 
 
-with open("README.md", "r") as f:
+with open("README.md", "r",encoding="utf-8") as f:
     long_description = f.read()
 
 
--- ytb_up/__init__.py
+++ ytb_up/__init__.py
@@ -1,5 +1,5 @@
 __name__ = "ytb-up"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from .exceptions import *
 from .constants import *
