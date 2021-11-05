84509f87aba7cdcccf62191d46e33593b617b592
wanghaisheng <edwin.uestc@gmail.com>
2021-11-05 23:52:49 +0800

    fix description xpath

--- ytb_up/constants.py
+++ ytb_up/constants.py
@@ -9,7 +9,7 @@ TAGS_CONTAINER = '//*[@id="tags-container"]'
 ERROR_CONTAINER = '//*[@id="error-message"]'
 STATUS_CONTAINER = "/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[2]/div/div[1]/ytcp-video-upload-progress/span"
 VIDEO_URL_CONTAINER = "//span[@class='video-url-fadeable style-scope ytcp-video-info']"
-DESCRIPTION_CONTAINER = "/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-basics/div[2]/ytcp-mention-textbox/ytcp-form-input-container/div[1]/div[2]/ytcp-mention-input"
+DESCRIPTION_CONTAINER = "//*[@id='description-container']"
 MORE_OPTIONS_CONTAINER = "/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-video-metadata-editor/div/div/ytcp-button"
 
 # COUNTERS
