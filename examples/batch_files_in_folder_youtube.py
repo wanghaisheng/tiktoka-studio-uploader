9b5f01f47d521ae718a6b51c0028c6f134385d09
wanghaisheng <edwin_uestc@163.com>
2023-08-24 19:43:29 +0800

    placehodler

--- examples/batch_files_in_folder_youtube.py
+++ examples/batch_files_in_folder_youtube.py
@@ -220,50 +220,6 @@ setting = {}
 setting["dailycount"] = 4
 
 
-def scheduletopublish_tomorrow(videopath, thumbpath, filename):
-    # mode a:release_offset exist,publishdate exist will take date value as a starting date to schedule videos
-    # mode b:release_offset not exist, publishdate exist , schedule to this specific date
-    # mode c:release_offset not exist, publishdate not exist,daily count to increment schedule from tomorrow
-    # mode d: offset exist, publish date not exist, daily count to increment with specific offset schedule from tomorrow
-    publish_date = datetime(today.year, today.month, today.day, 10, 15)
-    # if you want more delay ,just change 1 to other numbers to start from other days instead of tomorrow
-    # publish_date += timedelta(days=1)
-    asyncio.run(
-        upload.upload(
-            videopath,
-            title=filename[:95],
-            description=description,
-            thumbnail=thumbpath,
-            tags=prefertags,
-            release_offset="0-1",
-            closewhen100percentupload=True,
-            publishpolicy=2,
-            publish_date=publish_date,
-        )
-    )
-
-
-def scheduletopublish_7dayslater(videopath, thumbpath, filename):
-    # mode a:release_offset exist,publishdate exist will take date value as a starting date to schedule videos
-    # mode b:release_offset not exist, publishdate exist , schedule to this specific date
-    # mode c:release_offset not exist, publishdate not exist,daily count to increment schedule from tomorrow
-    # mode d: offset exist, publish date not exist, daily count to increment with specific offset schedule from tomorrow
-    publish_date = datetime(today.year, today.month, today.day, 10, 15)
-    # if you want more delay ,just change 1 to other numbers to start from other days instead of tomorrow
-    publish_date += timedelta(days=7)
-    asyncio.run(
-        upload.upload(
-            videopath,
-            title=filename[:95],
-            description=description,
-            thumbnail=thumbpath,
-            tags=prefertags,
-            release_offset="0-1",
-            closewhen100percentupload=True,
-            publishpolicy=2,
-            publish_date=publish_date,
-        )
-    )
 
 
 def scheduletopublish_specific_date(videopath, thumbpath, filename, publish_date):
@@ -287,10 +243,15 @@ def scheduletopublish_specific_date(videopath, thumbpath, filename, publish_date
             publish_date=publish_date,
         )
     )
-
+# scan video files
 
 videofiles = check_video_thumb_pair(videopath)
+# prepare youtube video metas
+## manual set each field
+## auto gen publish date 
+## parse meta from video file
 
+# save video metas to excel/json/sqlite
 
 videocount = len(videofiles)
 maxdays = calendar._monthlen(today.year, today.month)
